3218cde4057f76f6144669af7984ce86d47991a7
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-02 11:09:30 +0100

    Use quote routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from 'express';
-
+import quotesDB from './db';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -9,7 +9,11 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+// routes
+import quoteRoutes from './routes/quoteRoutes';
+app.use('/quotes', quoteRoutes);
 
 app.listen(port || 3000, () => {
+  quotesDB.generateQuotes();
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
