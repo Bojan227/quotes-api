4370abb69815e4f222851f815c087023b61f79fb
Bojan Blazevski <blazevski.bojan1@gmail.com>
2022-12-02 12:19:57 +0100

    Add /generate route for fetching fresh new quotes

--- src/routes/quoteRoutes.ts
+++ src/routes/quoteRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import { getQuotes, getRandomQuote } from '../controllers/quotesController';
+import {
+  getQuotes,
+  getRandomQuote,
+  generateNewQuotes,
+} from '../controllers/quotesController';
 const route = express.Router();
 
 route.get('/', getQuotes);
 route.get('/random-quote', getRandomQuote);
+route.get('/generate', generateNewQuotes);
 
 export default route;
